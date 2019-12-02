4971eaf5f2c1d2db56e1f158f046bfb8e0d1d72d
Your Real Name <me@example.com>
2019-12-01 19:03:21 -0600

    Third version of prime 2.

--- prime_number2.rb
+++ prime_number2.rb
@@ -6,6 +6,19 @@ def is_valid_digit?(string_of_digits, char)
   string_of_digits.include?(char)
 end
 
+# while index < a.size
+#   puts(a[index])
+
+#   # Compare a[index] with c
+
+#   if(a[index]) == c
+#     c_in_a = true
+#     break
+#   end
+
+#   index += 1
+# end
+
 def is_valid_number?(string_of_digits, input_string)
 
   # input_string is a string, can be 1 or multiple characters
@@ -21,41 +34,3 @@ end
 
 valid_number = is_valid_number?(string_of_digits, input_string)
 puts("#{input_string} is valid number : #{valid_number}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
